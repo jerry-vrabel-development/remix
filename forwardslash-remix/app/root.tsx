fc061c54d7c1ee08c8d55402281bdcd43a1cab47
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2022-12-01 21:39:59 -0600

    added scripts component, created prismic-remix app

--- forwardslash-remix/app/root.tsx
+++ forwardslash-remix/app/root.tsx
@@ -1,15 +1,20 @@
+import * as React from "react";
+import { Scripts } from "@remix-run/react";
 import { LiveReload } from "@remix-run/react";
 
 export default function App() {
+  const [count, setCount] = React.useState(0);
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <title>Remix: So great, it's funny!</title>
+        <title>Forwardslash Development</title>
       </head>
       <body>
-        Hello world
+        <h1>Hello world</h1>
+        <button onClick={() => setCount((c) => c + 1)}>{count}</button>
         <LiveReload />
+        <Scripts />
       </body>
     </html>
   );
